a8a367b1338accce565b7bd13062279a3226e9d8
Bibounet <bibou.ytb33@gmail.com>
2026-05-20 15:48:14 +0200

    hotfix:changed sql import cuz it's broken on windows

--- backend/Database.go
+++ backend/Database.go
@@ -5,13 +5,13 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 func CreateDatabase() {
 	os.MkdirAll("database", 0755)
 
-	db, err := sql.Open("sqlite3", "database/database.db")
+	db, err := sql.Open("sqlite", "database/database.db")
 	if err != nil {
 		log.Fatal(err)
 	}
