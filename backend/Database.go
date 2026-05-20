ddae6200712c7cb926c5de40e578d401e683efe4
Bibounet <bibou.ytb33@gmail.com>
2026-05-20 15:25:32 +0200

    automated database creation if not exists

--- backend/Database.go
+++ backend/Database.go
@@ -1,3 +1,25 @@
 package backend
 
-func main() {}
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func CreateDatabase() {
+	os.MkdirAll("database", 0755)
+
+	db, err := sql.Open("sqlite3", "database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connecté à database/database.db")
+}
