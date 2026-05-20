790f9a69bc2b7debbbf06ffb081f93f8565ddc1d
enzoandria1-oss <enzoandria1@gmail.com>
2026-05-20 16:02:22 +0200

    hotfix(backend/server): path of index file modified

--- backend/Server.go
+++ backend/Server.go
@@ -7,14 +7,14 @@ import (
 
 func Server() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../frontend/html/index.html")
+		http.ServeFile(w, r, "frontend/html/index.html")
 	})
 
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {	
-		http.ServeFile(w, r, "../frontend/html/register.html")
+		http.ServeFile(w, r, "frontend/html/register.html")
 	})
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../frontend/html/login.html")
+		http.ServeFile(w, r, "frontend/html/login.html")
 	})
 
 	fmt.Println("http://localhost:8080")
