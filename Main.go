c49cfad360cd68cd5a5752080c37d2d0600ef660
rlilou <lilou.rouquette@ynov.com>
2026-05-21 14:22:17 +0200

    feat(backend,frontend): add func for login and register

--- Main.go
+++ Main.go
@@ -6,6 +6,10 @@ import "forumjs/backend"
 func main() {
 	// Création de la base de données
 	backend.CreateDatabase()
+
+	// Création des tables
+	backend.CreateTables()
+	
 	// Démarrage du serveur
 	backend.Server()
 }
--- backend/Server.go
+++ backend/Server.go
@@ -6,16 +6,22 @@ import (
 )
 
 func Server() {
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("frontend/js"))))
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("frontend/css"))))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../frontend/html/index.html")
+		http.ServeFile(w, r, "frontend/html/index.html")
 	})
 
-	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {	
-		http.ServeFile(w, r, "../frontend/html/register.html")
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "frontend/html/register.html")
 	})
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../frontend/html/login.html")
+		http.ServeFile(w, r, "frontend/html/login.html")
 	})
+
+	http.HandleFunc("/db/register", register)
+	http.HandleFunc("/db/login", login)
 
 	fmt.Println("http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
